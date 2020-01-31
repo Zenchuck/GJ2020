460e73e429f9b7e0727f788b5f46bfdcf459eb84
Brayden Anderson <banderson.programmer@gmail.com>
2020-02-01 11:50:23 +1300

    trying to be able to pull

---
+++ Assets/Scripts/Character/MoveToClickPoint.cs
@@ -0,0 +1,29 @@
+﻿// MoveToClickPoint.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClickPoint : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform t;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        //agent.destination = t.transform.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 10000))
+            {
+                agent.destination = hit.point;
+                Debug.Log(hit.point);
+            }
+        }
+    }
+}
