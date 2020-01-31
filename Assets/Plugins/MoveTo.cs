77ea8f7d35ab62532a00bfb5f45c5ae7be87e3a3
Brayden Anderson <banderson.programmer@gmail.com>
2020-01-31 21:34:53 +1300

    Particle click
    Particle click

---
+++ Assets/Plugins/MoveTo.cs
@@ -0,0 +1,29 @@
+﻿// MoveToClickPoint.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public ParticleSystem click;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+                click.transform.position = new Vector3(hit.point.x, 0.1f, hit.point.z);
+                click.Play();
+            }
+        }
+    }
+}
